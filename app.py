04855c966deb8a2305987dd052c5f97952b3b687
DivaIrnanda <144981120+DivaIrnanda@users.noreply.github.com>
2025-06-03 15:16:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ if page == "home":
     st.markdown("""
     <div style='text-align: justify; font-size: 18px;'>
         <p>
-           <strong>SiDakin</strong> (Sistem Deteksi Kemiskinan Daerah) adalah solusi cerdas untuk memetakan tingkat kemiskinan di berbagai wilayah Indonesia. Aplikasi ini memanfaatkan teknologi data dan kecerdasan buatan untuk menganalisis, mengklasifikasikan, serta memvisualisasikan wilayah-wilayah berdasarkan tingkat kesejahteraannya.
+           <strong>SIDAKIN</strong> (Sistem Deteksi Kemiskinan Daerah) adalah solusi cerdas untuk memetakan tingkat kemiskinan di berbagai wilayah Indonesia. Aplikasi ini memanfaatkan teknologi data dan kecerdasan buatan untuk menganalisis, mengklasifikasikan, serta memvisualisasikan wilayah-wilayah berdasarkan tingkat kesejahteraannya.
     </div>
     """, unsafe_allow_html=True)
 
